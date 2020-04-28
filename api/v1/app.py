2d61903a2a973d67d7097e9bae9c6a9c730bbb3e
luismedinaeng <malf95@hotmail.com>
2020-04-27 19:49:51 -0500

    [Update]Manage not found resources

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,7 +4,7 @@ Main module of web app
 """
 from models import storage
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from os import getenv
 
 app = Flask(__name__)
@@ -12,7 +12,18 @@ app.register_blueprint(app_views)
 
 @app.teardown_appcontext
 def close_storage(log):
+    """
+    Close the connection created to storage
+    """
     storage.close()
+
+@app.errorhandler(404)
+def not_found(error):
+    """
+    Manage 'not found' exceptions
+    """
+    return make_response(jsonify(error="Not found"), 404)
+
 
 if __name__ == "__main__":
     host = '0.0.0.0' if getenv('HBNB_API_HOST') is None \
