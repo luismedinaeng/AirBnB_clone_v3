031b2c19472ba44daf9a5cb6a3a8bf37d653a52e
Nzparra <nzparra@gmail.com>
2020-04-28 22:29:40 -0500

    [Fix]Update method

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -72,6 +72,6 @@ def update_amenity(amenity_id):
         abort(400, description="Not a Json")
     for key, value in body.items():
         if key not in ['id', 'created_at', 'updated_at']:
-            setattr(amenitie, key, value)
+            setattr(amenity, key, value)
     storage.save()
     return make_response(jsonify(amenity.to_dict()), 200)
