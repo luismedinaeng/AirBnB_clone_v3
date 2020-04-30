c4d32f2e1680fa7e4412fbb7870c931a1e78be1c
luismedinaeng <malf95@hotmail.com>
2020-04-29 22:31:52 -0500

    [Add]Docs for index module

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -11,6 +11,22 @@ from models import storage
 def status():
     """
     Return a Json with status OK
+    Endpoint that check the status fo the API
+    ---
+    tags:
+      - index
+    produces:
+      - application/json
+    responses:
+      200:
+        description: Success connection
+        schema:
+          id: Status
+          properties:
+            status:
+              type: string
+              description: Status connection
+              example: OK
     """
     return jsonify(status="OK")
 
@@ -18,7 +34,44 @@ def status():
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def stats():
     """
-    Retrieves the number of each objects by type
+    Return the number of each objects by type
+    Return the number of the objects managed by the API such as Amenity,
+    City, Place, Review, State, User
+    ---
+    tags:
+      - index
+    produces:
+      - application/json
+    responses:
+      200:
+        description: Succesful operation
+        schema:
+          id: Stats
+          properties:
+            amenities:
+              type: int
+              description: Number of Amenity objects
+              example: 10
+            cities:
+              type: int
+              description: Number of City objects
+              example: 8
+            places:
+              type: int
+              description: Number of Place objects
+              example: 24
+            reviews:
+              type: int
+              description: Number of Review objects
+              example: 42
+            states:
+              type: int
+              description: Number of States objects
+              example: 3
+            users:
+              type: int
+              description: Number of User objects
+              example: 35
     """
     return jsonify(amenities=storage.count("Amenity"),
                    cities=storage.count("City"),
